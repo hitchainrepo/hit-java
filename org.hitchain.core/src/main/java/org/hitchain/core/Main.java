221fd4ce062db2a59cb37df9f7b3ccf5e974db53
tylerchen <iffiff1@gmail.com>
2019-04-19 16:07:35 +0800

    add hit command: repo member

--- org.hitchain.core/src/main/java/org/hitchain/core/Main.java
+++ org.hitchain.core/src/main/java/org/hitchain/core/Main.java
@@ -8,8 +8,14 @@
 package org.hitchain.core;
 
 import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.Repository;
+import org.hitchain.hit.api.ProjectInfoFile;
+import org.hitchain.hit.util.EthereumHelper;
+import org.hitchain.hit.util.GitHelper;
 import org.hitchain.hit.util.HitHelper;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.LinkedList;
 
@@ -53,7 +59,6 @@ import java.util.LinkedList;
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-        args = new String[]{"cfg", "account", "add", "main", "123456"};
         if (args != null && args.length > 0 && "cfg".equals(args[0])) {
             LinkedList<String> list = new LinkedList<>(Arrays.asList(args));
             list.poll();
@@ -61,8 +66,8 @@ public class Main {
                 HitHelper.hitConfigInfo(null);
                 return;
             }
-            String type = list.poll();
-            String operation = list.poll();//account, rsa, storage, repository
+            String type = list.poll();//account, rsa, storage, repository
+            String operation = list.poll();//add, remove, set
             String name = list.poll();
             String v1 = list.poll();// [priKey] [pubKey] password, url, null
             String v2 = list.poll();// [priKey] [pubKey] password, url, null
@@ -178,9 +183,153 @@ public class Main {
                 HitHelper.repositoryInfo(name);
                 return;
             }
+        }
+        if (args != null && args.length > 0 && "repo".equals(args[0])) {
+            File projectDir = null;
+            {
+                String workDir = System.getProperty("git_work_tree");
+                if (workDir == null) {
+                    workDir = ".";
+                    System.out.println(
+                            "System property 'git_work_tree' not specified, using current directory: "
+                                    + new File(workDir).getAbsolutePath());
+                }
+                projectDir = new File(workDir + "/.git");
+            }
+            if (projectDir == null || !projectDir.exists()) {
+                System.err.println(projectDir.getAbsolutePath() + " is invalid git dir.");
+                System.exit(1);
+            }
+            try (Repository db = new FileRepository(projectDir)) {
+                LinkedList<String> list = new LinkedList<>(Arrays.asList(args));
+                list.poll();
+                if (list.isEmpty()) {
+                    // list all members
+                    return;
+                }
+                String type = list.poll();//member
+                String operation = list.poll();//add, remove
+                String member = list.poll();//团队成员Email
+                String memberPubKeyRsa = list.poll();//成员的RSA公钥
+                String memberAddressEcc = list.poll();//成员的Ethereum地址
+                if (HitHelper.TYPE_help.equals(type)) {
+                    System.out.println("hit repo help");
+                    System.out.println("hit repo keypair   add");
+                    System.out.println("hit repo keypair   remove");
+                    System.out.println("hit repo keypair   renew");
+                    System.out.println("hit repo member    add    memberEmail publicRsa eccAddress");
+                    System.out.println("hit repo member    remove memberEmail");
+                    return;
+                }
+                ProjectInfoFile projectInfoFile = getProjectInfoFile(projectDir);
+                if (HitHelper.TYPE_member.equals(type)) {
+                    if ("add".equals(operation)) {
+                        addMember(projectDir, member, memberPubKeyRsa, memberAddressEcc, projectInfoFile);
+                        return;
+                    }
+                    if ("remove".equals(operation)) {
+                        if (removeMember(projectDir, member, memberAddressEcc, projectInfoFile)) return;
+                        return;
+                    }
+                    {// print team member info.
+                        printMemberInfo(projectInfoFile);
+                    }
+                    return;
+                }
+            }
         } else {
             Class<?> main = Class.forName("org.eclipse.jgit.pgm.Main");
-            main.getMethod(HitHelper.TYPE_main, String[].class).invoke(null, args);
+            main.getMethod(HitHelper.TYPE_main, String[].class).invoke(null, new Object[]{args});
         }
+    }
+
+    private static void printMemberInfo(ProjectInfoFile projectInfoFile) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Team members: \n");
+        for (ProjectInfoFile.TeamInfo ti : projectInfoFile.getMembers()) {
+            sb.append(StringUtils.rightPad(ti.getMember(), 20)).append(' ')
+                    .append(StringUtils.rightPad(ti.getMemberAddressEcc(), 45)).append(' ')
+                    .append(ti.getMemberPubKeyRsa()).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    private static boolean removeMember(File projectDir, String member, String memberAddressEcc, ProjectInfoFile projectInfoFile) {
+        ProjectInfoFile.TeamInfo teamInfo = null;
+        for (ProjectInfoFile.TeamInfo ti : projectInfoFile.getMembers()) {
+            if (StringUtils.equals(ti.getMember(), member)) {
+                teamInfo = ti;
+                break;
+            }
+        }
+        if (teamInfo == null) {
+            System.out.println("No member found.");
+            return true;
+        }
+        projectInfoFile.getMembers().remove(teamInfo);
+        String result = EthereumHelper.removeTeamMember(projectInfoFile.getEthereumUrl(), projectInfoFile.getRepoAddress(), memberAddressEcc);
+        if (EthereumHelper.isError(result)) {
+            System.err.println("Remove member error:" + result);
+            System.exit(1);
+        }
+        if (!GitHelper.updateHitRepositoryProjectInfoFile(projectDir, projectInfoFile)) {
+            System.err.println("Update project info file error.");
+            System.exit(1);
+        }
+        System.out.println("Member has removed in the contract.");
+        return false;
+    }
+
+    private static void addMember(File projectDir, String member, String memberPubKeyRsa, String memberAddressEcc, ProjectInfoFile projectInfoFile) {
+        if (StringUtils.isBlank(member) || StringUtils.isBlank(memberPubKeyRsa) || StringUtils.isBlank(memberAddressEcc)) {
+            System.err.println("Can't not execute command.");
+            System.exit(1);
+        }
+        for (ProjectInfoFile.TeamInfo ti : projectInfoFile.getMembers()) {
+            if (StringUtils.equals(ti.getMember(), member) || StringUtils.equals(ti.getMemberAddressEcc(), memberAddressEcc)) {
+                System.out.println("Member is exists.");
+                return;
+            }
+        }
+        if (projectInfoFile.isPrivate()) {
+            projectInfoFile.addMemberPrivate(member, memberPubKeyRsa, memberAddressEcc, HitHelper.getRsaPriKeyWithPasswordInput());
+        } else {
+            projectInfoFile.addMemberPublic(member, memberPubKeyRsa, memberAddressEcc);
+        }
+        if (EthereumHelper.hasTeamMember(projectInfoFile.getEthereumUrl(), projectInfoFile.getRepoAddress(), memberAddressEcc)) {
+            System.out.println("Member is exists in the contract.");
+            return;
+        }
+        String result = EthereumHelper.addTeamMember(projectInfoFile.getEthereumUrl(), projectInfoFile.getRepoAddress(), memberAddressEcc);
+        if (EthereumHelper.isError(result)) {
+            System.err.println("Add member error:" + result);
+            System.exit(1);
+        }
+        if (!GitHelper.updateHitRepositoryProjectInfoFile(projectDir, projectInfoFile)) {
+            System.err.println("Update project info file error.");
+            System.exit(1);
+        }
+        System.out.println("Member has added in the contract.");
+        return;
+    }
+
+    private static ProjectInfoFile getProjectInfoFile(File projectDir) {
+        ProjectInfoFile projectInfoFile = null;
+        if (GitHelper.existProjectInfoFile(projectDir)) {
+            projectInfoFile = GitHelper.readProjectInfoFile(projectDir);
+        }
+        if (projectInfoFile == null) {
+            System.err.println(projectDir.getAbsolutePath() + " is invalid hit repository.");
+            System.exit(1);
+        }
+        if (!StringUtils.equals(HitHelper.getAccountAddress(), projectInfoFile.getOwnerAddressEcc())) {
+            System.err.println("Only owner can change this settings.");
+            System.exit(1);
+        }
+        if (!projectInfoFile.verify(null)) {
+            System.err.println("Only owner can change this settings.");
+            System.exit(1);
+        }
+        return projectInfoFile;
     }
 }
